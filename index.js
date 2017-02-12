9da388498861821c7c11b955583ef5dad874be39
Abhilash K <abhilash1in@gmail.com>
2017-02-13 03:13:21 +0530

    updated API

--- index.js
+++ index.js
@@ -123,9 +123,8 @@ router.get('/patient', function(req, res) {
 });
 
 router.post('/ambulance/registertoken', function(req, res) {
-	var obj = JSON.parse(req.body);
-	var ambulance_id = obj.id;
-	var ambulance_token = obj.token;
+	var ambulance_id = req.body.id;
+	var ambulance_token = req.body.token;
 	console.log(req.body);
     var queryData = {
     	ambulance_id : ambulance_id,
